a3fde89b19b0d9ca8fa5822ef9d646dfeb6bf73e
michalfirszt <michalfiszu@gmail.com>
2021-11-11 11:19:09 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default App;
